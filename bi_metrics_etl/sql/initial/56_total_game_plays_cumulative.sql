4584dfa56f603eda57ca1ab5e1eff0cca7f19f9b
pramudithamendis <134496244+pramudithamendis@users.noreply.github.com>
2025-11-13 12:15:43 +0530

    56 fix

--- bi_metrics_etl/sql/initial/56_total_game_plays_cumulative.sql
+++ bi_metrics_etl/sql/initial/56_total_game_plays_cumulative.sql
@@ -5,7 +5,7 @@ SET @cutoff := '2025-09-27 18:30:00';
 -- Step 1: Insert cumulative total sessions
 INSERT INTO total_game_plays_cumulative (date_, total_sessions)
 SELECT 
-    DATE(CONVERT_TZ(d.created_at, '+00:00', '+08:00')) AS date_,
+    d.date_ AS date_,
     SUM(d.total_sessions) OVER (ORDER BY d.date_) AS total_sessions
 FROM total_game_plays_daily d
 ORDER BY d.date_;
