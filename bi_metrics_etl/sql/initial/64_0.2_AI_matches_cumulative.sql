c35e5b15dc293aa4aab6a74b0a634ad3383ddc16
pramudithamendis <134496244+pramudithamendis@users.noreply.github.com>
2025-11-21 16:34:41 +0530

    fixes

--- bi_metrics_etl/sql/initial/64_0.2_AI_matches_cumulative.sql
+++ bi_metrics_etl/sql/initial/64_0.2_AI_matches_cumulative.sql
@@ -6,7 +6,7 @@ date_,
 select distinct(t.date__),t.total_ai_matches,t.player_wins,t.player_losses,t.spend_amount_usd from
 (SELECT 
 d.date_ as date__,
-   COUNT(*) OVER (ORDER BY d.date_) AS total_ai_matches,
+SUM(d.total_ai_matches) OVER (ORDER BY d.date_) AS total_ai_matches,
    SUM(d.player_wins) OVER (ORDER BY d.date_) AS player_wins,
     SUM(d.player_losses) OVER (ORDER BY d.date_) AS player_losses,
     SUM(d.spend_amount_usd) OVER (ORDER BY d.date_) AS spend_amount_usd
