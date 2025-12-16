72b4440a6c58e44d81c2f1f4413502a8f7fa2095
pramudithamendis <134496244+pramudithamendis@users.noreply.github.com>
2025-12-16 16:50:53 +0530

    Update 102_user_leaderboard_total.sql

--- bi_metrics_etl/sql/initial/102_user_leaderboard_total.sql
+++ bi_metrics_etl/sql/initial/102_user_leaderboard_total.sql
@@ -1,13 +1,14 @@
 -- Current leaderboard
 
 -- truncate table gaming_app_bi.game_c_finish_c_won_c_earnings;
+
 CREATE TABLE gaming_app_bi.game_c_finish_c_won_c_earnings (
     user_id BIGINT NOT NULL,
 
     total_played_games_count INT NOT NULL DEFAULT 0,
     total_finished_count INT NOT NULL DEFAULT 0,
     total_won_count INT NOT NULL DEFAULT 0,
-    total_earnings BIGINT NOT NULL DEFAULT 0,
+    total_earnings DECIMAL(10,2) NOT NULL DEFAULT 0.00,
 
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -58,13 +59,13 @@ SELECT
     ) AS totalEarnings
 
 FROM
-    gaming_app_backend2.user_game_session a,
-    gaming_app_backend2.user u,
-    gaming_app_backend2.game_session gs,
-    gaming_app_backend2.game_session_mode gsm,
-    gaming_app_backend2.game_session_status gss,
-    gaming_app_backend2.user_coin_action uca,
-    gaming_app_backend2.user_coin_action_type ucat
+    gaming_app_backend.user_game_session a,
+    gaming_app_backend.user u,
+    gaming_app_backend.game_session gs,
+    gaming_app_backend.game_session_mode gsm,
+    gaming_app_backend.game_session_status gss,
+    gaming_app_backend.user_coin_action uca,
+    gaming_app_backend.user_coin_action_type ucat
 
 WHERE
     a.is_active = @isActive
@@ -84,9 +85,8 @@ GROUP BY
 
 select * from gaming_app_bi.game_c_finish_c_won_c_earnings;
 
-truncate table gaming_app_bi.user_leaderboard_total;
 select * from gaming_app_bi.user_leaderboard_total;
-
+truncate table gaming_app_bi.user_leaderboard_total;
 CREATE TABLE gaming_app_bi.user_leaderboard_total (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
 
